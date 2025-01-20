b2805a2e9454cee9a9f3bd5730b041c290a00f1b
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-20 01:26:54 -0300

    Fix renderer initialization check and update input handling logic in main.c
    - Changed the renderer initialization check to ensure it fails gracefully if the renderer is not created.
    - Updated input handling to correctly toggle the is_running state on SDL_QUIT and ESC key events.

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ bool initialize_window(void) {
   }
 
   renderer = SDL_CreateRenderer(window, -1, 0);
-  if (renderer) {
+  if (!renderer) {
     fprintf(stderr, "Error creating SDL renderer.\n");
     return false;
   }
@@ -29,17 +29,19 @@ bool initialize_window(void) {
   return true;
 }
 
+void setup(void) {}
+
 void process_input() {
   SDL_Event event;
   SDL_PollEvent(&event);
 
   switch (event.type) {
   case SDL_QUIT: // Quando clica no botão fechar
-    !is_running;
+    is_running = !is_running;
     break;
   case SDL_KEYDOWN:
     if (event.key.keysym.sym == SDLK_ESCAPE)
-      !is_running;
+      is_running = !is_running;
     break;
   }
 }
