706a1fcabb8d9ad91ec58210e47ae61114cf40c0
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-22 22:14:03 -0300

    Refactor window management and memory handling in main.c
    - Introduced constants for window dimensions to improve readability.
    - Allocated color buffer memory for rendering based on window size.
    - Implemented destroy_window() function to properly free resources and clean up SDL components.
    - Updated initialize_window() to use defined constants for window size.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 // Global Variables
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 bool is_running = false;
+uint32_t *color_buffer = NULL; // [0]
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -14,7 +19,7 @@ bool initialize_window(void) {
   }
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       600, 800, SDL_WINDOW_BORDERLESS);
+                       WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
   if (!window) {
     fprintf(stderr, "Error creating SDL window.\n");
     return false;
@@ -29,7 +34,9 @@ bool initialize_window(void) {
   return true;
 }
 
-void setup(void) {}
+void setup(void) {
+  color_buffer = (uint32_t *)(sizeof(uint32_t) * WINDOW_WIDTH * WINDOW_HEIGHT);
+}
 
 void process_input() {
   SDL_Event event;
@@ -54,6 +61,13 @@ void render(void) {
   SDL_RenderPresent(renderer);
 }
 
+void destroy_window(void) {
+  free(color_buffer);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
 int main(void) {
   is_running = initialize_window();
 
@@ -65,5 +79,6 @@ int main(void) {
     render();
   }
 
+  destroy_window();
   return 0;
 }
